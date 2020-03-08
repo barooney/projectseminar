74bdc4c4c8a4f1e9d31121e93b347981f85ebae6
Daniel Baron <daniel@barooney.com>
2020-03-08 13:05:19 +0100

    Changed bin sizes for creating test samples

--- create_small_test_sample.py
+++ create_small_test_sample.py
@@ -8,7 +8,7 @@ df_basis = pd.read_json('./data/intermediate/zipf_all_reviews.json', lines=True)
 
 # set bins
 max_funny = df_basis['funny'].max()
-bins =  [-1,0,1,2,5,9999999999999999]
+bins =  [-1,1,3,5,10,100,9999999999999999]
 labels=[number for number in range(1,len(bins))]
 
 #### assign bins
@@ -21,20 +21,20 @@ print("max reviews in highest bin: " + str(max_reviews_in_highest_bin))
 
 min_bin = 999999999999999
 
-asdf = dict()
+bin_separations = dict()
 
 for bin_number, num_reviews in tqdm(counter_obj.items()):
-	asdf[bin_number] = df_basis.query('funniness_category==@bin_number')
+	bin_separations[bin_number] = df_basis.query('funniness_category==@bin_number')
 	if df_basis.query('funniness_category==@bin_number').shape[0] < min_bin:
 		min_bin = df_basis.query('funniness_category==@bin_number').shape[0]
 
 print(str(min_bin))
-print(asdf)
+print(bin_separations)
 
 output_file = open('./data/intermediate/random-small_reviews.json', 'w')
-for e in asdf:
-	asdf[e] = asdf[e].sample(frac=1).head(2000)
-	for row in asdf[e].iterrows():
+for bin in bin_separations:
+	bin_separations[bin] = bin_separations[bin].sample(frac=1).head(2000)
+	for row in bin_separations[bin].iterrows():
 		row[1].to_json(output_file)
 		output_file.write("\n")
 		
