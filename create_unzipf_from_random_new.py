2c6f5de458fe40c7159d25624c48efccd13ab0a4
Daniel Baron <daniel@barooney.com>
2020-03-27 12:48:04 +0100

    create unzipfed data

---
+++ create_unzipf_from_random_new.py
@@ -0,0 +1,58 @@
+# imports
+
+# stand library
+from collections import Counter
+import os
+import sys
+import itertools
+import argparse
+
+# third party modules
+import reverse_geocoder
+import json
+import nltk
+from tqdm import tqdm
+
+# application specific
+from models import Review
+
+# DEFAULTS
+
+# define folder structure
+base_path = os.getcwd()
+data_path = base_path + '/data'
+intermediate_data_path = data_path + '/intermediate'
+
+try:
+    os.mkdir(data_path)
+    print("Folder created.")
+except FileExistsError:
+    print("Folder already exists.")
+
+try:
+    os.mkdir(intermediate_data_path)
+    print("Folder created.")
+except FileExistsError:
+    print("Folder already exists.")
+
+review_ids = []
+with open(data_path + '/intermediate/reviews_to_filter.txt', encoding='utf8') as review_ids_file:
+	for l in tqdm(review_ids_file.readlines()):
+		review_ids.append(l.strip())
+
+reviews = dict()
+
+# Load all reviews with respect to the given businesses
+with open(data_path + '/yelp/yelp_academic_dataset_review.json', encoding="utf8") as reviews_file:
+    for l in tqdm(reviews_file.readlines()):
+        r = Review(json.loads(l))
+        if r.review_id in review_ids:
+            reviews[r.review_id] = r
+
+print("# Reviews loaded: " + str(len(reviews.values())))
+
+reviews_intermediate_file = open(intermediate_data_path + '/' + "new_random_small_unzipfed.json", 'w')
+for r in tqdm(reviews):
+    json.dump(reviews[r].__dict__, reviews_intermediate_file)
+    reviews_intermediate_file.write("\n")
+reviews_intermediate_file.close()
--- find_languages.py
+++ find_languages.py
@@ -1,8 +1,5 @@
 from langdetect import detect
 import pandas as pd
-import dask.dataframe as dd
-from dask.multiprocessing import get
-from dask.distributed import progress
 from tqdm import tqdm
 from collections import Counter
 import numpy as np
@@ -10,22 +7,18 @@ import numpy as np
 tqdm.pandas()
 # raed all reviews with stop words for language detection
 df = pd.read_json('./data/yelp/' + 'yelp_academic_dataset_review.json', lines=True)
-# df = pd.read_json('./data/yelp/' + 'Illinois_reviews.json', lines=True)
-ddata = dd.from_pandas(df, npartitions=24)
 #df = pd.read_json('./data/intermediate/' + 'Illinois_reviews.json', lines=True)
 
-def find_lang(review_id, user_id, business_id, stars, useful, funny, cool, text, date):
+def find_lang(text):
     try:
         return detect(text)
     except:
         return "-no-lang-"
 
-# df["language_code"] = df['text'].progress_apply(lambda text_as_string: find_lang(text_as_string))
+df["language_code"] = df['text'].progress_apply(lambda text_as_string: find_lang(text_as_string))
 
-res = ddata.map_partitions(lambda df: df.apply((lambda row: find_lang(*row)), axis=1)).compute(scheduler='threads')
-print(res)
 
-list_lang_codes = res.tolist()
+list_lang_codes = df["language_code"].tolist()
 
 new_cnt = Counter(list_lang_codes)
 
